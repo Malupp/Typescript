4e30d62a95001e666e4617c7a680ea32d999441b
Manuel Luppino <60667104+Warlup@users.noreply.github.com>
2023-02-28 15:10:37 +0100

    exercise

---
+++ 06/exercise-1-define-object-types-start.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+// Add a type annotation to this variable that describes the object it contains.
+var countryPopulation = {
+    code: "NZ",
+    population: 5135300
+};
+;
